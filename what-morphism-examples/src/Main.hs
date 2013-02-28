a00f82d96d931a587dd70ea83b4fa9ee88b7a4cf
Jasper Van der Jeugt <m@jaspervdj.be>
2013-02-28 01:14:22 +0100

    Check scopes

--- what-morphism-examples/src/Main.hs
+++ what-morphism-examples/src/Main.hs
@@ -15,6 +15,13 @@ sum1 (x : xs) = x + sum1 xs
 
 
 --------------------------------------------------------------------------------
+-- | Kind of like sum, but we do some nasty scoping trick I guess
+scope :: [Int] -> Int
+scope []       = 0
+scope (x : xs) = x + scope xs + length xs
+
+
+--------------------------------------------------------------------------------
 map1 :: (a -> b) -> [a] -> [b]
 map1 _ []       = []
 map1 f (x : xs) = f x : map1 f xs
