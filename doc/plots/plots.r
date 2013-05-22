1526e4e74bdc1831edeff3e06e8cb6dafaa37e76
Jasper Van der Jeugt <m@jaspervdj.be>
2013-05-22 13:58:14 +0200

    Plot confidence intervals

--- doc/plots/plots.r
+++ doc/plots/plots.r
@@ -1,3 +1,5 @@
+require(plotrix)
+
 enabled  <- read.csv(file="enabled.csv", header=T)
 disabled <- read.csv(file="disabled.csv", header=T)
 
@@ -14,17 +16,31 @@ barplot(tree_speedups,
 dev.off()
 
 pdf("tree.pdf")
-plot(seq(1, 5), tree_unfused,
+plotCI(seq(1, 5), tree_unfused,
+        enabled[1:5, 'StddevUB'] * 2,
+        enabled[1:5, 'StddevLB'] * 2,
         pch=0,
         ylim=c(0, max(tree_unfused)),
         xlab="Benchmark",
         ylab="Tijd (in s)",
+        col='red',
         axes=F)
-points(seq(1, 5), tree_fused, pch=1)
+plotCI(seq(1, 5), tree_fused,
+        enabled[1:5, 'StddevUB'] * 2,
+        enabled[1:5, 'StddevLB'] * 2,
+        pch=1,
+        col='blue',
+        add=T)
 axis(side=1, at=seq(1, 5), labels=tree_names, lwd=0)
 axis(side=2)
-legend("topleft", legend=c("Zonder fusion", "Met fusion"), pch=c(0, 1))
+legend("topleft", legend=c("Zonder fusion", "Met fusion"), pch=c(0, 1),
+       col=c('red', 'blue'))
 dev.off()
+
+# Copy pasta. s/tree/list/g. s/1:5/6:10
+
+enabled  <- read.csv(file="enabled.csv", header=T)
+disabled <- read.csv(file="disabled.csv", header=T)
 
 list_names    <- enabled[6:10, 'Name']
 list_fused    <- enabled[6:10, 'Mean']
@@ -39,14 +55,23 @@ barplot(list_speedups,
 dev.off()
 
 pdf("list.pdf")
-plot(seq(1, 5), list_unfused,
+plotCI(seq(1, 5), list_unfused,
+        enabled[6:10, 'StddevUB'] * 2,
+        enabled[6:10, 'StddevLB'] * 2,
         pch=0,
         ylim=c(0, max(list_unfused)),
         xlab="Benchmark",
         ylab="Tijd (in s)",
+        col='red',
         axes=F)
-points(seq(1, 5), list_fused, pch=1)
+plotCI(seq(1, 5), list_fused,
+        enabled[6:10, 'StddevUB'] * 2,
+        enabled[6:10, 'StddevLB'] * 2,
+        pch=1,
+        col='blue',
+        add=T)
 axis(side=1, at=seq(1, 5), labels=list_names, lwd=0)
 axis(side=2)
-legend("topleft", legend=c("Zonder fusion", "Met fusion"), pch=c(0, 1))
+legend("topleft", legend=c("Zonder fusion", "Met fusion"), pch=c(0, 1),
+       col=c('red', 'blue'))
 dev.off()
